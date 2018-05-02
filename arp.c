6bd3ac843dd3ce5cfc4e86730b9506d74003df47
Kyle <dahnky@mnstate.edu>
2018-05-01 21:04:14 -0500

    Created and populated arp.c

---
+++ arp.c
@@ -0,0 +1,44 @@
+/*
+ * Kernel code to send and receive ARP requests and responses
+ */
+#include "types.h"
+#include "defs.h"
+#include "arpfrm.h"
+#include "nic.h"
+
+static int blockuntilreply(struct eth_head * eth) {
+    /*
+     * TODO: Sleep until woken up by network interrupt.
+     * 		check for ARP reply with this request.
+     * 		If reply is received unblock, otherwise sleep.
+     */
+    return 0;
+}
+
+int sendrequest(char * interface, char * ipadd, char * arpresp) {
+    cprintf("Create ARP request for IP:%s over Interface:%s\n", ipadd, interface);
+    
+    // Test if the NIC is found/connected/loaded
+    struct nic_device * nic;
+    if (getdevice(interface, &nic) < 0) {
+	cprintf("ERROR: sendrequest : Device not loaded\n");
+	return -1;
+    }
+    
+    // Create an ARP packet and send it across the NIC
+    struct eth_head eth;
+    initframe(nic->mac, ipadd, &eth);
+    nic->sendpacket(nic->driver, (uint*_t *) &eth, sizeof(eth) - 2);	// Removing the padding
+    
+    // Initialize the ARP response and test if it exists
+    struct eth_head resp;
+    if (blockuntilreply(&resp) < 0) {
+	cprintf("ERROR: sendrequest : Device not loaded\n");
+	return -3;
+    }
+    
+    unpackmac(resp.arpdmac, arpresp);
+    arpresp[17] = '\0';
+    
+    return 0;
+}
